01116d6e0c3482e3cee3e5af99a2c6fe20d11401
Maxime521 <9842@holbertonstudents.com>
2024-10-22 08:28:04 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,2 +1,21 @@
-#include
-gcc -o positive_or_negative -Wall -Werror -Wextra -pedantic -std=gnu89 0-positive_or_negative.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(0)); // Seed the random number generator
+    int n = rand() % (2 * RAND_MAX + 1) - RAND_MAX; // Random number in the range [-RAND_MAX, RAND_MAX]
+
+    printf("The number is: %d\n", n);
+    
+    if (n > 0) {
+        printf("is positive\n");
+    } else if (n == 0) {
+        printf("is zero\n");
+    } else {
+        printf("is negative\n");
+    }
+
+    return 0;
+}
+
