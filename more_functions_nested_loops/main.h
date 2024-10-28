dc881349153e52f12677ea2dbeacda0f597c1b08
Maxime521 <9842@holbertonstudents.com>
2024-10-28 10:50:53 +0000

    update

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the more_functions_nested_loops directory.
+ */
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
