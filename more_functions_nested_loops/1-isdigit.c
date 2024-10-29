961b4af055f2189fd9c062288084f9712b9e5bc8
Maxime521 <9842@holbertonstudents.com>
2024-10-29 09:56:10 +0000

    1-isdigit.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -8,7 +8,12 @@
  * Return: 1 if the number is a digit, 0 otherwise.
  */
 
-int _isdigit(int c) {
-    return (c >= '0' && c <= '9') ? 1 : 0;
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+
+		return (1);
+	else
+		return (0);
 }
 
