4446901cbba7dc38d90dd5f82bfc2bf03eb5f0a9
Maxime521 <9842@holbertonstudents.com>
2024-11-01 17:37:27 +0000

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,13 @@
+#include "main.h"
 
+/**
+ * reset_to_98 - as parameter and updates the value it points
+ * @int: Write a function that takes a pointer
+ */
 
+void reset_to_98(int *n)
+{
 
+	*n = 98;
+
+}
