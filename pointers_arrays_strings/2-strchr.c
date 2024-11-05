55bd7d80d2cd114ab0dc598da122ea98ebf60aac
Maxime521 <9842@holbertonstudents.com>
2024-11-05 09:38:16 +0000

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
   * _strchr - locate character in string
@@ -7,24 +7,23 @@
   *
   * Return: the string from character found
   */
-
 char *_strchr(char *s, char c)
 {
 	int a = 0, b;
 
 	while (s[a])
 	{
-	a++;
+		a++;
 	}
 
-	for (b = 0; b < a; b++)
+	for (b = 0; b <= a; b++)
 	{
-	if (c == s[b])
-	{
-	s += b;
-	return (s);
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
 	}
 
-	}
 	return ('\0');
 }
