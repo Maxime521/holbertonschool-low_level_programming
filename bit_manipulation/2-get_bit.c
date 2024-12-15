9c4deffd4d0f933a4b0d377047747aa636bffe63
Maxime521 <9842@holbertonstudents.com>
2024-12-15 14:06:30 +0000

    Create the all files and input the code in my pc

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - return value of a bit at a given value
+ * @n: number received
+ * @index: index wanted
+ * Return: value of the number at the index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int lol;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	lol = 1 << index;
+return ((n & lol) != 0);
+}
